914a90ea0ae71234c9662d9db06c59b06d1cda68
Babatunde Ishau <ishautunde700@gmail.com>
2025-10-25 23:01:47 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production'
-    ? '/your-repo-name/' 
-    : '/',               
+    ? '/Vue-Ticket-Web-App/' 
+    : '/',                   
 })
