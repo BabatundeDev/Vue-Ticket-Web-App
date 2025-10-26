2f50f69e319cbfd83e0c6d53b61b99de9e9189ef
Babatunde Ishau <ishautunde700@gmail.com>
2025-10-26 01:25:33 +0100

    initial-commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [vue()],
-    base: env.VITE_BASE || '/',
-  }
+export default defineConfig({
+  plugins: [vue()],
+  base: '/', 
 })
